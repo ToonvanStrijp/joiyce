5f80b5a202edd2c5412f75334cf0d120fb47df55
Toon Van Strijp <toonvanstrijp@toons-macbook-pro.local>
2018-05-25 22:36:50 +0200

    Initial Commit

---
+++ Joiyce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Joiyce
+//
+//  Created by Toon Van Strijp on 25/05/2018.
+//  Copyright © 2018 Toon Van Strijp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JoiyceTests/JoiyceTests.swift
@@ -0,0 +1,36 @@
+//
+//  JoiyceTests.swift
+//  JoiyceTests
+//
+//  Created by Toon Van Strijp on 25/05/2018.
+//  Copyright © 2018 Toon Van Strijp. All rights reserved.
+//
+
+import XCTest
+@testable import Joiyce
+
+class JoiyceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
